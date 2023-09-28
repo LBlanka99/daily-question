dd5ca96e5c1fd91ca88da92d222d8e7b804a6010
LBlanka99 <losonci.blanka@mensa.hu>
2023-09-28 17:03:32 +0200

    create database connection

--- Entities/Program.cs
+++ Entities/Program.cs
@@ -1,4 +1,18 @@
+using Entities.Models.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables()
+    .Build();
+
+builder.Services.AddDbContext<DailyQuestionContext>(options =>
+    options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");

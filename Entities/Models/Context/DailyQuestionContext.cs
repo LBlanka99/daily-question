4aa282f46e073ef5fc2e7cf7b83b742e11303ce3
LBlanka99 <losonci.blanka@mensa.hu>
2023-09-28 17:05:09 +0200

    configure entity relationships in OnModelCreating

--- Entities/Models/Context/DailyQuestionContext.cs
+++ Entities/Models/Context/DailyQuestionContext.cs
@@ -15,4 +15,36 @@ public class DailyQuestionContext : DbContext
     public DbSet<QuestionModel> QuestionModels { get; set; }
     public DbSet<ReactionModel> ReactionModels { get; set; }
     public DbSet<UserModel> UserModels { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<FriendsModel>()
+            .HasKey(fr => new { fr.SenderUserId, fr.ReceiverUserId });
+
+        modelBuilder.Entity<FriendsModel>()
+            .HasOne(fr => fr.SenderUser)
+            .WithMany()
+            .HasForeignKey(fr => fr.SenderUserId);
+
+        modelBuilder.Entity<FriendsModel>()
+            .HasOne(fr => fr.ReceiverUser)
+            .WithMany()
+            .HasForeignKey(fr => fr.ReceiverUserId);
+
+        modelBuilder.Entity<ReactionModel>()
+            .HasKey(ra => new { ra.AnswerId, ra.UserId });
+
+        modelBuilder.Entity<ReactionModel>()
+            .HasOne(ra => ra.Answer)
+            .WithMany(a => a.Reactions)
+            .HasForeignKey(ra => ra.AnswerId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<ReactionModel>()
+            .HasOne(ra => ra.User)
+            .WithMany()
+            .HasForeignKey(ra => ra.UserId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+    }
 }

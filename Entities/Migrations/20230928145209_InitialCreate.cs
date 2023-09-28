ee5a01fdfa0cf421285df8b0ec07da480ff3faf7
LBlanka99 <losonci.blanka@mensa.hu>
2023-09-28 17:05:57 +0200

    update the database

---
+++ Entities/Migrations/20230928145209_InitialCreate.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entities.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "QuestionModels",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Text = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QuestionModels", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserModels",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserName = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Streak = table.Column<int>(type: "integer", nullable: false),
+                    ProfilePicture = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserModels", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Commentable",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Text = table.Column<string>(type: "text", nullable: false),
+                    Created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Discriminator = table.Column<string>(type: "text", nullable: false),
+                    QuestionId = table.Column<Guid>(type: "uuid", nullable: true),
+                    Visibility = table.Column<int>(type: "integer", nullable: true),
+                    ParentId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Commentable", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Commentable_Commentable_ParentId",
+                        column: x => x.ParentId,
+                        principalTable: "Commentable",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Commentable_QuestionModels_QuestionId",
+                        column: x => x.QuestionId,
+                        principalTable: "QuestionModels",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Commentable_UserModels_UserId",
+                        column: x => x.UserId,
+                        principalTable: "UserModels",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FriendsModels",
+                columns: table => new
+                {
+                    SenderUserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ReceiverUserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FriendsModels", x => new { x.SenderUserId, x.ReceiverUserId });
+                    table.ForeignKey(
+                        name: "FK_FriendsModels_UserModels_ReceiverUserId",
+                        column: x => x.ReceiverUserId,
+                        principalTable: "UserModels",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_FriendsModels_UserModels_SenderUserId",
+                        column: x => x.SenderUserId,
+                        principalTable: "UserModels",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ReactionModels",
+                columns: table => new
+                {
+                    AnswerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Type = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReactionModels", x => new { x.AnswerId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_ReactionModels_Commentable_AnswerId",
+                        column: x => x.AnswerId,
+                        principalTable: "Commentable",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ReactionModels_UserModels_UserId",
+                        column: x => x.UserId,
+                        principalTable: "UserModels",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Commentable_ParentId",
+                table: "Commentable",
+                column: "ParentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Commentable_QuestionId",
+                table: "Commentable",
+                column: "QuestionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Commentable_UserId",
+                table: "Commentable",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FriendsModels_ReceiverUserId",
+                table: "FriendsModels",
+                column: "ReceiverUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReactionModels_UserId",
+                table: "ReactionModels",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FriendsModels");
+
+            migrationBuilder.DropTable(
+                name: "ReactionModels");
+
+            migrationBuilder.DropTable(
+                name: "Commentable");
+
+            migrationBuilder.DropTable(
+                name: "QuestionModels");
+
+            migrationBuilder.DropTable(
+                name: "UserModels");
+        }
+    }
+}

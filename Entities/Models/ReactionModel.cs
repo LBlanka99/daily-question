66c0f35ba3b74443b2589eedfe3a974e904c981b
LBlanka99 <losonci.blanka@mensa.hu>
2023-09-28 17:02:22 +0200

    use AnswerId+UserId as primary key in ReactionModel.cs

--- Entities/Models/ReactionModel.cs
+++ Entities/Models/ReactionModel.cs
@@ -1,13 +1,26 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Entities.Enums;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Entities.Models;
 
 public class ReactionModel
 {
     [Key]
-    public Guid Id { get; set; }
-    public AnswerModel Answer { get; set; }
-    public UserModel User { get; set; }
+    [Column(Order = 1)]
+    public Guid AnswerId { get; set; }
+    
+    [Key]
+    [Column(Order = 2)]
+    public Guid UserId { get; set; }
+    
     public ReactionType Type { get; set; }
+    
+    [ForeignKey(nameof(AnswerId))]
+    public AnswerModel Answer { get; set; }
+    
+    [ForeignKey(nameof(UserId))]
+    public UserModel User { get; set; }
+    
 }

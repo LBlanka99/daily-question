c546dbb9cc2497ae9e2a1e41c0ec4391c12b0157
LBlanka99 <losonci.blanka@mensa.hu>
2023-10-10 17:46:58 +0200

    extend UserModel

---
+++ Entities/Migrations/20231003120858_extendUserModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entities.Migrations
+{
+    /// <inheritdoc />
+    public partial class extendUserModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "UserModel",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "UserModel");
+        }
+    }
+}

a17195ed9da9f647ebb74dde9b4d7565d68d916a
LBlanka99 <losonci.blanka@mensa.hu>
2023-10-10 17:49:40 +0200

    verify sign up data & send it to the server

--- DailyQuestion/ClientApp/src/pages/SignUp.tsx
+++ DailyQuestion/ClientApp/src/pages/SignUp.tsx
@@ -1,16 +1,49 @@
 import "./SignUp.css";
-import React, {useState} from "react";
+import React, {FormEvent, useState} from "react";
 import { Link, useNavigate } from "react-router-dom";
+import {Console} from "inspector";
 
 const SignUp = () => {
     const [data, setData]: any = useState(null);
 
     const navigate = useNavigate();
 
-    async function sendRegisterInfo(e: React.FormEvent) {
+    async function sendRegisterInfo(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault();
         // verify data
-        // send data to a server
+        const form = new FormData(e.target as HTMLFormElement);
+        const entries : [string, FormDataEntryValue][] = [...form.entries()];
+
+        const user = entries.reduce((acc: any, entry) => {
+            const [k, v] = entry;
+            acc[k] = v;
+            return acc;
+        }, {});
+
+
+        if (user.password !== user.passwordAgain) {
+            setData("password error");
+            return;
+        }
+
+        // send data to server
+        const apiAddress = "http://localhost:5194/api/v1/users";
+        
+        const body = JSON.stringify({
+            UserName: user.username,
+            Email: user.email,
+            Password: user.password,            
+        });
+        
+        console.log(body);
+
+        const init: RequestInit = {
+            method: "POST",
+            headers: new Headers([["content-type", "application/json"]]),
+            body: body,
+        };
+
+        const response = await fetch(apiAddress, init);
 
     }
 
@@ -53,11 +86,16 @@ const SignUp = () => {
                 <input
                     type="password"
                     id="password-again"
-                    name="password-again"
+                    name="passwordAgain"
                     placeholder="password again"
                     required
                 />
             </div>
+            {data === "password error" ? (
+                <span className="error">The two passwords don't match!</span>
+            ) : (
+                <></>
+            )}
             <button type="submit">Sign Up</button>
         </form>
     </div>
